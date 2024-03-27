6fd4a8c2c11a00b960bf241142a145eb3cc67826
agus <agusalta002@gmail.com>
2024-03-27 19:47:02 -0300

    add modal

---
+++ index.js
@@ -0,0 +1,43 @@
+// Seleccionar elementos relevantes
+let previewContainer = document.querySelector('.cuadros-preview');
+let previewBoxes = previewContainer.querySelectorAll('.preview');
+let cuadroContainers = document.querySelectorAll('.galeria-container .cuadro-container');
+
+// Agregar evento de clic a cada contenedor de cuadro
+cuadroContainers.forEach(cuadro => {
+  cuadro.addEventListener('click', () => {
+    // Mostrar la vista previa
+    previewContainer.style.display = 'flex';
+
+    // Ocultar todas las vistas previas
+    previewBoxes.forEach(preview => {
+      preview.style.display = 'none';
+    });
+
+    // Obtener el nombre del cuadro clickeado
+    let name = cuadro.getAttribute('data-name');
+
+    // Mostrar la vista previa correspondiente al cuadro clickeado
+    let targetPreview = previewContainer.querySelector(`.preview[data-target="${name}"]`);
+    if (targetPreview) {
+      targetPreview.style.display = 'block';
+    }
+
+    // Agregar evento de clic al botón de cierre dentro de la vista previa
+    let closeButton = targetPreview.querySelector('.cerrar-modal');
+    if (closeButton) {
+      closeButton.addEventListener('click', () => {
+        targetPreview.style.display = 'none';
+        previewContainer.style.display = 'none';
+      });
+    }
+  });
+});
+
+// Agregar evento de clic al botón de cierre de la vista previa
+previewBoxes.forEach(preview => {
+  preview.querySelector('.fa-times').addEventListener('click', () => {
+    preview.style.display = 'none';
+    previewContainer.style.display = 'none';
+  });
+});
